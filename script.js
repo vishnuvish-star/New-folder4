9dd8d8ffc7dfa73e93025a9c5360869aebca34f3
kesavan <kesavanplk7@gmail.com>
2023-02-22 02:12:05 -0500

    functions and array

--- script.js
+++ script.js
@@ -113,3 +113,87 @@
 //     billPayment + tip
 //   }`
 // );
+// functions
+// const nation = "india";
+// const peoples = "110";
+// const cityOfNation = "delhi";
+// const nationOfUk = "England";
+// const peoplesOfUk = "56";
+// const cityOfNationOfUk = "London";
+// function describeCountry(country, population, capitalCity) {
+//   return console.log(
+//     `${country} has ${population} million people and its capital city is ${capitalCity}`
+//   );
+// }
+// describeCountry(nation, peoples, cityOfNation);
+// describeCountry(nationOfUk, peoplesOfUk, cityOfNationOfUk);
+
+// function total(numOne, numTwo) {
+//   return console.log(numOne + numTwo);
+// }
+// total(2, 5);
+
+// function percentageOfWorld1(population) {
+//   const percentage = (population / 7900) * 100;
+//   return console.log(` China has ${population} million people, so it's about ${percentage} of
+// the world population`);
+// }
+// percentageOfWorld1(1101);
+// const percentageOfWorld3 = (population) => {
+//   const percentage = (population / 7900) * 100;
+//   return console.log(` India has ${population} million people, so it's about ${percentage} of
+// the world population`);
+// };
+// percentageOfWorld3(2113);
+
+// const dolphinsScore = (44 + 23 + 71) / 3;
+// const koalasScore = (65 + 54 + 49) / 3;
+// const calcAverage = () => {
+//   if (dolphinsScore >= 2 * koalasScore) {
+//     console.log("The winner is dolphins");
+//   } else {
+//     console.log("The winner is dolphins");
+//   }
+// };
+// FUNCTION CALLING
+// calcAverage();
+//TEST DATA #2
+
+// const dolphinsScoreTwo = (85 + 54 + 41) / 3;
+// const koalasScoreTwo = (23 + 34 + 27) / 3;
+// const averageOfSecondGame = () => {
+//   if (dolphinsScoreTwo >= 2 * koalasScoreTwo) {
+//     console.log("Dolphins are winner of this game");
+//   } else {
+//     console.log("Kolas are winner of this game");
+//   }
+// };
+// // CALL THE FUNCTION
+// averageOfSecondGame();
+
+// ARRAY
+// const populations = [110, 228, 48, 49, 67, 354];
+// console.log(populations.length);
+// // const calculateLength = populations.length - 1;
+// // console.log(calculateLength);
+// if (populations.length === 6) {
+//   console.log("Populations array length is equal 6");
+// } else {
+//   console.log("Populations array length lesser than  6");
+// }
+
+// ARRAY METHODS
+
+// const neighbours = ["SRI LANKA", "BANGALADESH", "PAKISTAN", "WEST INDIES"];
+// console.log(neighbours); //original array
+
+// const neighboursAdd = neighbours.push("Utopia", "africa"); //push method add data end of an arry
+// console.log(neighboursAdd);
+
+// const neighboursAddStart = neighbours.unshift("india", "nepal"); //Add the data from starting position
+// console.log(neighboursAddStart);
+
+// const neighboursRemove = neighbours.pop(); //pop method remove a data ,end of the array and its kept only remove a particular data
+// console.log(neighboursRemove);
+// const neighboursRemoveStart = neighbours.shift(); //remove the data from starting
+// console.log(neighboursRemoveStart);
